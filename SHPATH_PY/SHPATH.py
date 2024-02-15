b6bfc074f453ba69092a90201b3fdaaccfae6dd8
Roger Banks <roger_banks@mac.com>
2024-02-15 15:34:11 -0600

    Update SHPATH.py
    Clear graph between tests. Still getting TLE.

--- SHPATH_PY/SHPATH.py
+++ SHPATH_PY/SHPATH.py
@@ -9,28 +9,25 @@ def edges(graph, u, v):
 def dijkstra(graph, source):
     q = PriorityQueue()
     dist = {}
-    prev = {}
     dist[source] = 0
     q.put((0, source))
     for v in graph:
         if v != source:
             dist[v] = math.inf
-            prev[v] = None
     while not q.empty():
         u = q.get()
         for v in graph[u[1]]:
             alt = dist[u[1]] + edges(graph, u[1], v)
             if alt < dist[v]:
                 dist[v] = alt
-                prev[v] = u[1]
                 q.put((alt, v))
-    return dist, prev
+    return dist
 
-graph = {}
 num_tests = sys.stdin.readline().strip()
 for _ in range(int(num_tests)):
     num_cities = sys.stdin.readline().strip()
     cities = []
+    graph = {}
     for _ in range(int(num_cities)):
         city_name = sys.stdin.readline().strip()
         num_neighbors = sys.stdin.readline()
@@ -52,7 +49,7 @@ for _ in range(int(num_tests)):
         path = path.split()
         fromCity = path[0]
         toCity = path[1]
-        dist, prev = dijkstra(graph, fromCity)
+        dist = dijkstra(graph, fromCity)
 
         print(dist[toCity])
     sys.stdin.readline()
